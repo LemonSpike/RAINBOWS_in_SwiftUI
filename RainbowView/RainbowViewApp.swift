7b9a9db4cdbcc0fb292313de66ccf9c05f9edac3
Pranav Kasetti <pranavkasetti@gmail.com>
2021-04-05 17:19:05 +0100

    Initial Commit

---
+++ RainbowView/RainbowViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  RainbowViewApp.swift
+//  RainbowView
+//
+//  Created by Pranav Kasetti on 05/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct RainbowViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
