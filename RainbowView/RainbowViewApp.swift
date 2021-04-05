66de2444238d447cef22df24fce01f3a87c6e632
Pranav Kasetti <pranavkasetti@gmail.com>
2021-04-05 17:32:58 +0100

    Initial commit.

--- RainbowView/RainbowViewApp.swift
+++ RainbowView/RainbowViewApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct RainbowViewApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      RainbowView(size: .init(width: 300, height: 300))
     }
+  }
 }
